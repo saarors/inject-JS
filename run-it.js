86946bd04be2c1ca51a1c69221e8d3f03456b2ed
saar-IL <saarleshembar@gmail.com>
2026-03-31 14:38:58 +0300

    Load external script and handle load events

--- run-it.js
+++ run-it.js
@@ -1,3 +1,7 @@
 const script = document.createElement('script');
 script.src = "https://cdn.jsdelivr.net/gh/saarors/inject-JS@main/source.js";
+
+script.onload = () => console.log("Script loaded!");
+script.onerror = () => console.log("Failed to load script");
+
 document.head.appendChild(script);
