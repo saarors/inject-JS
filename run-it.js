17d350cc6b1bcd9dd885c3b15291c979153316b5
saar-IL <saarleshembar@gmail.com>
2026-04-01 13:33:28 +0300

    Update script source URL to specific commit

--- run-it.js
+++ run-it.js
@@ -1,5 +1,5 @@
 const script = document.createElement('script');
-script.src = "https://cdn.jsdelivr.net/gh/saarors/inject-JS@main/source.js";
+script.src = "https://cdn.jsdelivr.net/gh/saarors/inject-JS@08f8cf6914959273316256aed0f1983a8089f029/source.js";
 
 script.onload = () => console.log("Script loaded!");
 script.onerror = () => console.log("Failed to load script");
